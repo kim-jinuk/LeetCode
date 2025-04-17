16090719211c1c779ff3b87169b8dce21e7529ef
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-04-17 11:06:22 +0900

    Time: 7 ms (74.04%), Space: 21.7 MB (15.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,15 +5,16 @@ class Solution:
         i = 0
         n = len(intervals)
         while (i <= n - 1):
+            max_value = intervals[i][1]
             if (i == n - 1):
                 answer.append([intervals[i][0], intervals[i][1]])
                 return answer
 
             j = i + 1
-            while (j < n and intervals[i][1] >= intervals[j][0]):
-                intervals[i][1] = max(intervals[i][1], intervals[j][1])
+            while (j < n and max_value >= intervals[j][0]):
+                max_value = max(max_value, intervals[j][1])
                 j += 1
-            answer.append([intervals[i][0], intervals[i][1]])
+            answer.append([intervals[i][0], max_value])
             i = j
 
         return answer
