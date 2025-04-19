be62f06d85d4340a2c21a6b92a0a84a4132f91b0
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-04-19 16:06:47 +0900

    Time: 539 ms (28.21%), Space: 20.3 MB (32.35%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -7,7 +7,7 @@ class Solution:
         effort = collections.defaultdict(int)
         move = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-        while 1:
+        while Q:
             e, x, y = heapq.heappop(Q)
             if x == row - 1 and y == column - 1:
                 return e
