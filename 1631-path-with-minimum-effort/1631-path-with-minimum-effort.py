ba0ebda3f08312cc905d2dddd78d0ae16e8af3bc
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-04-19 16:25:53 +0900

    Time: 551 ms (27.81%), Space: 20.4 MB (20.84%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -11,11 +11,10 @@ class Solution:
             e, x, y = heapq.heappop(Q)
             if x == row - 1 and y == column - 1:
                 return e
+
             if (x, y) not in effort:
                 effort[(x, y)] = e
                 for dx, dy in move:
                     if 0 <= x + dx < row and 0 <= y + dy < column:
                         value = max(heights[x + dx][y + dy] - heights[x][y], heights[x][y] - heights[x + dx][y + dy])
                         heapq.heappush(Q, (max(e, value), x + dx, y + dy))
-        
-        return Q[0][0]
