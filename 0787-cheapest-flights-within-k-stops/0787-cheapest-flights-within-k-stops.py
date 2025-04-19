0280260c0787228865b7c9e1c9a396da96a5d767
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-04-19 23:27:19 +0900

    Time: 15 ms (53.93%), Space: 19.1 MB (69.76%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,36 +1,27 @@
-import collections, heapq
-from typing import List
+import collections
+import heapq
 
 class Solution:
-    def findCheapestPrice(
-        self,
-        n: int,
-        flights: List[List[int]],
-        src: int,
-        dst: int,
-        K: int
-    ) -> int:
-        # 그래프 구성
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = collections.defaultdict(list)
-        for u, v, w in flights:
-            graph[u].append((v, w))
+        for a, b, c in flights:
+            graph[a].append((b, c))
 
-        # best[도시][경유횟수] = 최소 비용
-        best = [[float('inf')] * (K + 2) for _ in range(n)]
+        best = [[float('inf')] * (k + 2) for _ in range(n)]
         best[src][0] = 0
 
-        pq = [(0, src, 0)]  # (비용, 도시, 사용한 경유 횟수)
-        while pq:
-            cost, u, stops = heapq.heappop(pq)
-            if u == dst:
-                return cost
-            if stops == K + 1 or cost > best[u][stops]:
+        Q = [(0, src, 0)]
+        while Q:
+            p, s, cnt = heapq.heappop(Q)
+            if s == dst:
+                return p
+            if cnt == k + 1 or best[s][cnt] < p:
                 continue
-            for v, w in graph[u]:
-                nc = cost + w
-                # stops+1번 경유해서 v에 올 때 비용이 더 싸면 갱신
-                if nc < best[v][stops + 1]:
-                    best[v][stops + 1] = nc
-                    heapq.heappush(pq, (nc, v, stops + 1))
+            for a, b in graph[s]:
+                nx = p + b
+                if best[a][cnt + 1] > nx:
+                    best[a][cnt + 1] = nx
+                    heapq.heappush(Q, (nx, a, cnt + 1))
 
         return -1
+                
