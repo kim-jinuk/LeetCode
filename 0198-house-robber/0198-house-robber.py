f56ff222fb595e909e28fad2142a52b7581ffa69
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-04-26 17:52:45 +0900

    Time: 3 ms (8.3%), Space: 17.6 MB (82.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -9,7 +9,7 @@ class Solution:
         
         dp = collections.OrderedDict()
         dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-        
+
         for i in range(2, len(nums)):
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
         
